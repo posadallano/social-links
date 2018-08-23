2179c108754a948fafb419a332867e5bf44f96dc
Andres Posada <posadallano2@gmail.com>
2018-08-22 22:12:51 -0500

    Initial commit

---
+++ social-links.php
@@ -0,0 +1,17 @@
+<?php 
+/**
+* Plugin Name: Social Links
+* Description: Adds a social links widget
+* Version: 1.0
+* Author: Andres Posada
+**/
+
+// Exit if Accessed Directly
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+// Load scripts
+require_once( plugin_dir_path( __FILE__ ) . 'includes/social-links-scripts.php' );
+
+?>
